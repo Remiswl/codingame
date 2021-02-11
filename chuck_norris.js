f24d34ca4e0280d87120c52b57b54043e110843b
Remiswl <r.schwall@hotmail.fr>
2021-02-11 17:35:18 +0100

    :art: add chuck_norris.js - first step (convert text to binary)

---
+++ chuck_norris.js
@@ -0,0 +1,15 @@
+// 1. Convert the message text into binaty code
+const MESSAGE = readline().charCodeAt().toString(2);
+
+console.log(MESSAGE);
+
+// 2.
+
+
+console.log('answer');
+
+
+// How to convert text to binary code:
+// 1. Convert every char using charCodeAt function to get the Ascii Code in decimal (an integer between 0 and 65535) (UTF-16 code)
+// 2. Convert it to Binary value (base 2) using toString(2)
+// text/decimal/binary conversion: https://www.rapidtables.com/code/text/ascii-table.html
