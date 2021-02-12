517a191bc0ade939cdf9968e456ac2f1c85095fc
Remiswl <r.schwall@hotmail.fr>
2021-02-12 17:14:25 +0100

    :art: update chuck_norris.js

--- chuck_norris.js
+++ chuck_norris.js
@@ -1,31 +1,44 @@
-const MESSAGE = readline().charCodeAt().toString(2);
+const binaryMessage = readline().charCodeAt().toString(2);
+let arrayMessage = [];
+let unaireMessage = '';
 
-console.log(MESSAGE);
-
-let array = [];
-
-for (let i = 0; i < MESSAGE.length; i++) {
-    array.push(parseInt(MESSAGE[i]));
+for (let i = 0; i < binaryMessage.length; i++) {
+    arrayMessage.push(parseInt(binaryMessage[i]));
 }
 
-console.log(array);
+// Pour chacun des éléments du tableau,
+for (let i = 0; i < arrayMessage.length; i++) {
+    console.log(arrayMessage[i]);
 
-for (let j = 0; j < array.length; j++){
-    let integersCount;
-
-    if (array[j] == 1) {
-
+    // on identifie le premier élément
+    if (arrayMessage[i] == 0) {
+        unaireMessage = `${unaireMessage }${00 }`;
+    } else {
+        unaireMessage = `${unaireMessage }${0 }`;
     }
+
+    // puis on continue à lire le tableau
+    // dès qu'on change de chiffre, on arrête la boucle
+    // et on compte le nombre d'occurence du nombre
+    let bitCount = 0;
+
+    for (let j = 0; j < arrayMessage.length; j++) {
+        console.log(arrayMessage[j], arrayMessage [i]);
+
+        // while (arrayMessage[j] = arrayMessage [i]) {
+        //    bitCount ++;
+        // }
+    }
+
+    console.log(bitCount);
+    // puis on reprend la boucle
 }
 
-function (){
-
-}
-
-console.log('answer');
+console.log(arrayMessage);
+console.log(unaireMessage);
 
 
 // How to convert text to binary code:
-// 1. Convert every char using charCodeAt function to get the Ascii Code in decimal (an integer between 0 and 65535) (UTF-16 code)
-// 2. Convert it to Binary value (base 2) using toString(2)
-// text/decimal/binary conversion: https://www.rapidtables.com/code/text/ascii-table.html
+// 1. Convert every char using charCodeAt function to get the Ascii Code in decimal (UTF-16 code)
+// 2. Convert it to Binary value using toString(2)
+// check the result on https://www.rapidtables.com/code/text/ascii-table.html
