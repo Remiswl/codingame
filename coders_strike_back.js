82c33fe379235ada6e87148b4c14371512427733
Remiswl <r.schwall@hotmail.fr>
2021-02-15 16:44:40 +0100

    :art: add coders_strike_back - Wood league 1

---
+++ coders_strike_back.js
@@ -0,0 +1,24 @@
+while (true) {
+    var inputs = readline().split(' ');
+    const x = parseInt(inputs[0]);
+    const y = parseInt(inputs[1]);
+    const nextCheckpointX = parseInt(inputs[2]);
+    const nextCheckpointY = parseInt(inputs[3]);
+    const nextCheckpointDist = parseInt(inputs[4]);
+    const nextCheckpointAngle = parseInt(inputs[5]);
+    var inputs = readline().split(' ');
+    const opponentX = parseInt(inputs[0]);
+    const opponentY = parseInt(inputs[1]);
+
+    let thrust;
+
+    if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+        thrust = 0;
+    } else if (nextCheckpointDist > 3000) {
+        thrust = 'BOOST';
+    } else {
+        thrust = 100;
+    }
+
+    console.log(`${nextCheckpointX } ${ nextCheckpointY } ${ thrust}`);
+}
