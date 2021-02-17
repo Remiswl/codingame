7953ce438d9295772b539de49463e4e9914a340c
Remiswl <r.schwall@hotmail.fr>
2021-02-17 14:22:25 +0100

    :art: remove comments

--- la_descente.js
+++ la_descente.js
@@ -1,10 +1,9 @@
-// game loop
 while (true) {
     let highestMountain = 0;
     let mountainFire = 0;
 
     for (let i = 0; i < 8; i++) {
-        const mountainH = parseInt(readline()); // represents the height of one mountain.
+        const mountainH = parseInt(readline());
 
         if (mountainH > highestMountain) {
             highestMountain = mountainH;
@@ -12,7 +11,5 @@ while (true) {
         }
     }
 
-    // Write an action using console.log()
-    // To debug: console.error('Debug messages...');
-    console.log(mountainFire); // The index of the mountain to fire on.
+    console.log(mountainFire);
 }
--- power_of_thor.js
+++ power_of_thor.js
@@ -1,20 +1,18 @@
 var inputs = readline().split(' ');
-const lightX = parseInt(inputs[0]); // the X position of the light of power
-const lightY = parseInt(inputs[1]); // the Y position of the light of power
-const initialTx = parseInt(inputs[2]); // Thor's starting X position
-const initialTy = parseInt(inputs[3]); // Thor's starting Y position
+const lightX = parseInt(inputs[0]);
+const lightY = parseInt(inputs[1]);
+const initialTx = parseInt(inputs[2]);
+const initialTy = parseInt(inputs[3]);
 
 let thorCurrentPositionX = initialTx;
 let thorCurrentPositionY = initialTy;
 
-// game loop
 while (true) {
     let moveX;
     let moveY;
 
-    const remainingTurns = parseInt(readline()); // The remaining amount of turns Thor can move. Do not remove this line.
-    // Write an action using console.log()
-    // To debug: console.error('Debug messages...');
+    const remainingTurns = parseInt(readline());
+
     if (thorCurrentPositionX < lightX) {
         moveX = 'E';
         thorCurrentPositionX++;
@@ -35,6 +33,5 @@ while (true) {
         moveY = '';
     }
 
-    // A single line providing the move to be made: N NE E SE S SW W or NW
     console.log(moveY + moveX);
 }
