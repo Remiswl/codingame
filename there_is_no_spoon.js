b6f2ef902f39374605dbcaf0981a1045fe05fb88
Remiswl <r.schwall@hotmail.fr>
2021-02-22 08:25:35 +0100

    :art: update - 1st test passed

--- there_is_no_spoon.js
+++ there_is_no_spoon.js
@@ -2,27 +2,36 @@ const width = parseInt(readline());
 const height = parseInt(readline());
 
 let x1, y1, x2, y2, x3, y3;
-
+let table = [];
 
 for (let i = 0; i < height; i++) {
     const line = readline();
+    for (let j = 0; j < width; j++) {
+        if (line[j] == '0') {
+            x1 = j;
+            y1 = i;
+        }
 
-    if (line[i] == '0') {
-        x1 = i;
-        y1 = 0;
+        if (line[j + 1] != undefined && line[j + 1] == '0') {
+            x2 = j + 1;
+            y2 = i;
+        /* else if (line[j+1] == '.') {
+            ???????
+        }*/
+        else if (line[j+1] == undefined) {
+            x2 = -1;
+            y2 = -1;
+        }
+
+        if (line[i + 1 + j] == '0') {
+            x3 = j;
+            y3 = i + 1;
+        } else if (line[i + 1 + j] == undefined || line[i + 1 + j] == '.') {
+            x3 = -1;
+            y3 = -1;
+        }
+
+        let answer = ` ${ x1 } ${ y1 } ${ x2 } ${ y2 } ${ x3 } ${ y3}`;
+        console.log(answer);
     }
-
-    if (line[i + 1] != undefined && line[i + 1] == '0') {
-        x2 = i + 1;
-        y2 = 0;
-    } else if (line[i + 1] != undefined && line[i + 1] == '.') {
-        x2 = -1;
-        y2 = -1;
-    }
-
-    x3 = 0;
-    y3 = 1;
-
-    let answer = ` ${ x1 } ${ y1 } ${ x2 } ${ y2 } ${ x3 } ${ y3}`;
-    console.log(answer);
 }
