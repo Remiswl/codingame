2818587e214d992a2eac1ac0cf82c9e6b3a0db34
Remiswl <r.schwall@hotmail.fr>
2021-02-26 15:38:30 +0100

    :art: update defebrillateurs.js

--- defebrillateurs.js
+++ defebrillateurs.js
@@ -1,13 +1,30 @@
-const LON = readline();
-const LAT = readline();
+const LON = parseFloat(readline().replace(',', '.'));
+const LAT = parseFloat(readline().replace(',', '.'));
 const N = parseInt(readline());
 
-let x = (longB - longA) * cos((latA + latB) / 2);
-let y = latB - latA;
-let d = Math.sqrt(x * x + y * y) * 6371;
+let distToUser;
+let shortestDist;
+let closestDef;
 
-for (let i = 0; i < N; i++) {
-    const DEFIB = readline();
+function calcDistance(longA, latA, longB, latB) {
+    let x = (longB - longA) * Math.cos((latA + latB) / 2);
+    let y = latB - latA;
+    return Math.sqrt(x * x + y * y) * 6371;
 }
 
-console.log('answer');
+for (let i = 0; i < N; i++) {
+    const DEFIB = readline().split(';');
+
+    distToUser = calcDistance(LON, LAT, parseFloat(DEFIB[4].replace(',', '.')), parseFloat(DEFIB[5].replace(',', '.'))).toFixed(3);
+
+    if (shortestDist == undefined) {
+        shortestDist = distToUser;
+    }
+
+    if (distToUser < shortestDist) {
+        distToUser = shortestDist;
+        closestDef = DEFIB[1];
+    }
+}
+
+console.log(closestDef);
