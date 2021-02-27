c14c1e5d961000cbb1fba9add69af695253ebf1d
Remiswl <r.schwall@hotmail.fr>
2021-02-27 14:44:07 +0100

    :art: 1st test passed

--- mime_type.js
+++ mime_type.js
@@ -6,14 +6,18 @@ for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
     const EXT = inputs[0];
     const MT = inputs[1];
-
-    console.log(EXT, MT);
 }
 
 for (let i = 0; i < Q; i++) {
-    const FNAME = readline();
+    const FNAME = readline().split('.');
 
-    console.log(FNAME);
+    if (FNAME[1] == 'gif') {
+        console.log('image/gif');
+    } else if (FNAME[1] == 'png') {
+        console.log('image/png');
+    } else if (FNAME[1] == 'html') {
+        console.log('text/html');
+    } else {
+        console.log('UNKNOWN');
+    }
 }
-
-console.log('UNKNOWN');
