c887836af7305d06af3c5103a666a960172411ba
Remiswl <r.schwall@hotmail.fr>
2021-03-01 09:32:10 +0100

    :art: 70% success

--- mime_type.js
+++ mime_type.js
@@ -1,22 +1,31 @@
 const N = parseInt(readline());
 const Q = parseInt(readline());
-
+let mimeTypes = [];
 
 for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
-    const EXT = inputs[0];
+    const EXT = inputs[0].toLowerCase();
     const MT = inputs[1];
+
+    mimeTypes.push(EXT, MT);
 }
 
 for (let i = 0; i < Q; i++) {
-    const FNAME = readline().split('.');
+    let FNAME = readline();
+    let dot = FNAME.lastIndexOf('.');
 
-    if (FNAME[1] == 'gif') {
-        console.log('image/gif');
-    } else if (FNAME[1] == 'png') {
-        console.log('image/png');
-    } else if (FNAME[1] == 'html') {
-        console.log('text/html');
+    if (FNAME.lastIndexOf('.') >= 0 && FNAME.slice(dot + 1).length > 0) {
+        FNAME = FNAME.slice(dot + 1);
+
+        for (let j = 0; j < mimeTypes.length; j++) {
+            if (FNAME.toLowerCase() == mimeTypes[j].toLowerCase()) {
+                console.log(mimeTypes[j + 1]);
+            }
+        }
+
+        if (mimeTypes.includes(FNAME.toLowerCase()) === false) {
+            console.log('UNKNOWN');
+        }
     } else {
         console.log('UNKNOWN');
     }
