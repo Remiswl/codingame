9a34ec003a76f195a0f1336b146ec227a93b060d
Remiswl <r.schwall@hotmail.fr>
2021-03-02 18:29:49 +0100

    :art: update -1st test: Timeout

--- there_is_no_spoon.js
+++ there_is_no_spoon.js
@@ -15,42 +15,68 @@ for (let i = 0; i < height; i++) {
     array.push(coord);
 }
 
-/*
-Tests:
-    1: array = [['0', '0'],['0','.']];
-    2: array = [[ '0', '.', '0', '.', '0' ]];
-    3: array = [[ '0' ], [ '0' ], [ '0' ], [ '0' ]];
-    4: array = [[ '0', '.', '0' ], [ '.', '.', '.' ], [ '0', '.', '0' ]];
-    5: array = [['0', '0', '0' ], [ '.', '0', '.' ], [ '.', '0', '.' ]];
-    6: ;
-    7: ;
-    8: ;
-*/
+function showCoords(i, j) {
+    x1 = j;
+    y1 = i;
+}
+
+function coordsRight(i, j) {
+    if (array[i] !== undefined && array[i][j + 1] !== undefined) {
+        if (array[i][j + 1] === '0') {
+            x2 = j + 1;
+            y2 = i;
+        } else if (array[i][j + 1] !== '0') {
+            do {
+                j = j + 1;
+            } while (array[i] !== undefined && array[i][j + 1] !== '0');
+
+            if (array[i] === undefined) {
+                x2 = -1;
+                y2 = -1;
+            } else if (array[i][j + 1] === '0') {
+                x2 = j + 1;
+                y2 = i;
+            }
+        }
+    } else {
+        x2 = -1;
+        y2 = -1;
+    }
+}
+
+function coordsBottom(i, j) {
+    if (array[i + 1] !== undefined && array[i + 1][j] !== undefined) {
+        if (array[i + 1][j] === '0') {
+            x3 = j;
+            y3 = i + 1;
+        } else if (array[i + 1][j] !== '0') {
+            do {
+                i = i + 1;
+            } while (array[i + 1] !== undefined && array[i + 1][j] !== '0');
+
+            if (array[i + 1] === undefined) {
+                x3 = -1;
+                y3 = -1;
+            } else if (array[i + 1][j] === '0') {
+                x3 = j + 1;
+                y3 = i;
+            }
+        }
+    } else {
+        x3 = -1;
+        y3 = -1;
+    }
+}
 
 for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
-        if (array[j][i] === '0') {
-            x1 = j;
-            y1 = i;
-        }
+        if (array[i][j] === '0') {
+            showCoords(i, j);
+            coordsRight(i, j);
+            coordsBottom(i, j);
 
-        if (array[j + 1] !== undefined && array[j + 1][i] === '0') {
-            x2 = j + 1;
-            y2 = i;
-        } else if (array[j + 1] === undefined || array[j + 1][i] !== '0') {
-            x2 = -1;
-            y2 = -1;
+            let answer = ` ${ x1 } ${ y1 } ${ x2 } ${ y2 } ${ x3 } ${ y3}`;
+            console.log(answer);
         }
-
-        if (array[i + 1] !== undefined && array[j][i + 1] === '0') {
-            x3 = j;
-            y3 = i + 1;
-        } else if (array[i + 1] === undefined || array[j][i + 1] !== '0') {
-            x3 = -1;
-            y3 = -1;
-        }
-
-        let answer = ` ${ x1 } ${ y1 } ${ x2 } ${ y2 } ${ x3 } ${ y3}`;
-        console.log(answer);
     }
 }
