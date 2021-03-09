bd831d66277b3406219454fa297efa88d151c730
Remiswl <r.schwall@hotmail.fr>
2021-03-09 16:18:23 +0100

    :art: 1st test passed

--- ascii_art.js
+++ ascii_art.js
@@ -1,13 +1,30 @@
 const L = parseInt(readline());
 const H = parseInt(readline());
-const T = readline();
+const T = readline().toUpperCase();
+let indexArray = [];
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ?';
+let k;
+let index;
 
-console.log(L, H, T);
-
-for (let i = 0; i < H; i++) {
-    const ROW = readline();
-
-    console.log(ROW[0]);
+for (let j = 0; j < T.length; j++) {
+    for (let i = 0; i < alphabet.length; i++) {
+        if (T[j] === alphabet[i]) {
+            indexArray.push(i);
+        }
+    }
 }
 
-console.log('answer');
+for (let j = 0; j < indexArray.length; j++) {
+    for (let i = 0; i < H; i++) {
+        let ROW = readline();
+
+        if (indexArray[j] !== 0) {
+            index = indexArray[j] * L;
+        } else {
+            index = 0;
+        }
+
+        k = ROW.substring(index, index + L);
+        console.log(k);
+    }
+}
