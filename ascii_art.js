24a0b6df0976f38a4ef91407b984469569eabb8b
Remiswl <r.schwall@hotmail.fr>
2021-03-09 11:41:36 +0100

    :art: add Ascii Art

---
+++ ascii_art.js
@@ -0,0 +1,13 @@
+const L = parseInt(readline());
+const H = parseInt(readline());
+const T = readline();
+
+console.log(L, H, T);
+
+for (let i = 0; i < H; i++) {
+    const ROW = readline();
+
+    console.log(ROW[0]);
+}
+
+console.log('answer');
