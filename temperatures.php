ca59c3516f8f7c88831044c79df807f6598ab22d
Remiswl <r.schwall@hotmail.fr>
2021-03-12 13:23:14 +0100

    :art: 90% success - still miss two negative temp. that are equal

--- temperatures.php
+++ temperatures.php
@@ -2,22 +2,20 @@
 fscanf(STDIN, "%d", $n);
 $inputs = explode(" ", fgets(STDIN));
 
-if($n == 0){
-    $result = 0;
-} else {
-    $result = max($inputs);
-}
+$result = ($n === 0) ? 0 : max($inputs);
 
 for ($i = 0; $i < $n; $i++) {
     $t = intval($inputs[$i]);
 
-    for($i=0; $i<sizeof($inputs);$i++) {
+    for($i = 0; $i < sizeof($inputs); $i++) {
         $t = $inputs[$i];
 
         if(abs($t) < abs($result)) {
             $result = $t;
-        } else if (abs($t) == abs($result) && $t != $result) {
+        } else if (abs($t) === abs($result) && $t !== $result) {
             $result = abs($t);
+        } else if ($t === $result && $t < 0) {
+            $result = $t;
         }
     }
 }
